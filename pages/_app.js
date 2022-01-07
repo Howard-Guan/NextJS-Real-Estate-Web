4f525cdec705020a78160b2d77d58e05cd9b4874
Howard Guan <gzhljh@gmail.com>
2022-01-07 16:02:55 -0700

    fix itemID props

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <ChakraProvider>
         <Layout>
